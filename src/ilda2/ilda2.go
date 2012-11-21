c2d6ef4b93ca043899bec6a3e50563170f8ea244
Sergey Yarmonov <sergey.yarmonov@gmail.com>
2012-11-21 18:58:36 +0700

    Реализована функция main 	* Реализована раздача заданий для функции конвертирования 	* Реализовано отображение прогресса конвертирования файла и сообщений

--- src/ilda2/ilda2.go
+++ src/ilda2/ilda2.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 const (
@@ -10,15 +11,52 @@ const (
 	CONVERT_ERROR = 2
 )
 
-// TODO: GIT
 func main() {
-	opt, _ := readOpts("ilda2.json")
-
-	// clear target dir
-	if err := os.RemoveAll(opt.TargetDir); err != nil {
-		fail(CONVERT_ERROR, "Unable remove all from target dir: ", err)
+	opt, err := readOpts("ilda2.json")
+	if err != nil {
+		fail(CONFIG_ERROR, "Unable read options:", err)
 	}
 
+	// clear target dir
+	if err = os.RemoveAll(opt.TargetDir); err != nil {
+		fail(CONVERT_ERROR, "Unable remove all from target dir:", err)
+	}
+
+	// convert files
+	for i, _ := range opt.Files {
+		for stat := range ilda2wavGo(&opt.Files[i], opt.TargetDir) {
+			if stat.err != nil {
+				fmt.Println()
+				fail(CONVERT_ERROR, "Unable convert file",
+					opt.Files[i].Name, stat.err)
+			}
+			status(&opt.Files[i], stat.percent)
+		}
+		fmt.Println()
+	}
+}
+
+func goToZeroPos() {
+	fmt.Print("\x1b[0G")
+}
+
+func printProgress(p int) {
+	fmt.Printf("[%s%s] %3d%%",
+		strings.Repeat("-", p/2),
+		strings.Repeat(" ", 50-p/2),
+		p)
+}
+
+func status(opt *FileConvOpt, persent int) {
+	const WIDTH = 15
+
+	name := opt.Name
+	if len(name) > WIDTH {
+		name = name[:WIDTH-3] + "..."
+	}
+	goToZeroPos()
+	fmt.Printf("%15s ", name)
+	printProgress(persent)
 }
 
 func check(err error, code int) {
